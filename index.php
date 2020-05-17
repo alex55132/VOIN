e9a8e391791c14dcc0d0f36477cfad33e8a51637
BuildTools <unconfigured@null.spigotmc.org>
2020-05-17 13:41:45 +0200

    programacion de la tienda con la disposicion de los productos y una ventana para el producto individualmente

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         <ul class="navContainer">
             <li class="navItem"><a href="#">Home</a></li>
             <li class="navItem"><a href="#">Browse</a></li>
-            <li class="navItem"><a href="#">Tienda</a></li>
+            <li class="navItem"><a href="tienda.php">Tienda</a></li>
             <li class="navItem"><a href="#">Subir video</a></li>
 
             <li id="loginBtn" class="navItem logBtn"><a href="#">Log-in / Registro</a></li>
