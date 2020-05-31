fbcb008e7c989c6b91caec2999a1a782d9d09f58
alex55132 <registrostope@gmail.com>
2020-05-31 12:14:14 +0200

    Minor fixes

--- tienda.php
+++ tienda.php
@@ -16,7 +16,7 @@ $lista->obtenerElementos();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>VOIN - Tienda</title>
     <link rel="stylesheet" href="css/navTienda.css">
     <link rel="stylesheet" href="css/tiendaStyle.css">
     <script src="js/tiendaScript.js"></script>
