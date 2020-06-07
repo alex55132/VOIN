ee90df3521c5cd1950608965c414b7d9bb0ba828
alex55132 <registrostope@gmail.com>
2020-06-07 13:59:38 +0200

    Gestion de reportes en el panel de control, aceptacion y rechazo de videos reportados

--- paneldecontrol/js/controlPanelScript.js
+++ paneldecontrol/js/controlPanelScript.js
@@ -4,6 +4,9 @@ let admTag;
 let gestTag;
 
 
+let acceptVideoBtn;
+let rejectVideoBtn;
+
 window.addEventListener('load', function () {
     //Cargamos las pestañas y le agregamos los listeners
     videoTag = document.getElementById("videoTag");
@@ -32,6 +35,64 @@ window.addEventListener('load', function () {
     if(gestTag != null) {
         gestTag.addEventListener("click", function () {
             window.location.href = "./panelDeControl.php?pestana=4";
+        });
+    }
+
+    acceptVideoBtn = document.getElementById("acceptVideoBtn");
+    if(acceptVideoBtn != null) {
+        acceptVideoBtn.addEventListener("click", function () {
+            let acceptPetition = new XMLHttpRequest();
+
+            acceptPetition.open("POST", "../Controllers/manageReportsController.php");
+
+            let acceptPetitionData = new FormData();
+            acceptPetitionData.append("typeRequest", "1");
+            acceptPetitionData.append("videoId", acceptVideoBtn.dataset.acceptedvideo)
+
+            acceptPetition.onreadystatechange = function () {
+                if(acceptPetition.readyState === 4) {
+                    if (acceptPetition.status === 200) {
+                        let JSONData = JSON.parse(acceptPetition.responseText);
+                        if(JSONData.statusCode === 1) {
+                            alert(JSONData.message);
+                            location.reload();
+                        } else {
+                            alert(JSONData.message);
+                        }
+                    }
+                }
+            }
+
+            acceptPetition.send(acceptPetitionData);
+        });
+    }
+
+    rejectVideoBtn = document.getElementById("rejectVideoBtn");
+    if(rejectVideoBtn != null) {
+        rejectVideoBtn.addEventListener("click", function () {
+            let rejectPetition = new XMLHttpRequest();
+
+            rejectPetition.open("POST", "../Controllers/manageReportsController.php");
+
+            let rejectPetitionData = new FormData();
+            rejectPetitionData.append("typeRequest", "2");
+            rejectPetitionData.append("videoId", rejectVideoBtn.dataset.rejectedvideo);
+
+            rejectPetition.onreadystatechange = function () {
+                if(rejectPetition.readyState === 4) {
+                    if (rejectPetition.status === 200) {
+                        let JSONData = JSON.parse(rejectPetition.responseText);
+                        if(JSONData.statusCode === 1) {
+                            alert(JSONData.message);
+                            location.reload();
+                        } else {
+                            alert(JSONData.message);
+                        }
+                    }
+                }
+            }
+
+            rejectPetition.send(rejectPetitionData);
         });
     }
 });
--- paneldecontrol/panelDeControl.php
+++ paneldecontrol/panelDeControl.php
@@ -22,7 +22,7 @@ if (isDataAvailable($_GET)) {
     <link rel="stylesheet" href="css/navPanelControl.css">
     <link rel="stylesheet" href="css/panelDeControlStyle.css">
     <?php
-    if(isDataAvailable($pestana)) {
+    if (isDataAvailable($pestana)) {
         if ($pestana == 4) {
             //Mostramos el script de gestion
             echo "<script src='js/gestionControlPanelScript.js'></script>";
@@ -56,7 +56,7 @@ require_once "../includes/navbarPanelControl.php";
                     break;
                 case 1:
                     //Moderador
-                    if($pestana == 2) {
+                    if ($pestana == 2) {
                         echo '<div id="videoTag" class="itemVideos">
                             <h1>VIDEOS</h1>
                         </div>
@@ -74,7 +74,7 @@ require_once "../includes/navbarPanelControl.php";
                     break;
                 case 2:
                     //Administrador
-                    if($pestana == 2) {
+                    if ($pestana == 2) {
                         echo '<div id="videoTag" class="itemVideos">
                             <h1>VIDEOS</h1>
                         </div>
@@ -100,7 +100,7 @@ require_once "../includes/navbarPanelControl.php";
                         <div id="gestTag" class="itemVideos">
                             <h1>GESTION</h1>
                         </div>';
-                    } else if($pestana == 4) {
+                    } else if ($pestana == 4) {
                         echo '<div id="videoTag" class="itemVideos">
                             <h1>VIDEOS</h1>
                         </div>
@@ -149,16 +149,16 @@ require_once "../includes/navbarPanelControl.php";
                         break;
                     case 2:
                         //Moderacion
-                        echo '<h3 class="moderacionTableElement">Video</h3>
-                                <h3 class="moderacionTableElement">Titulo</h3>
-                                <h3 class="moderacionTableElement">Autor</h3>';
+                        echo '<h3 class="moderacionTableElementHeader">Video</h3>
+                                <h3 class="moderacionTableElementHeader">Titulo</h3>
+                                <h3 class="moderacionTableElementHeader">Autor</h3>';
                         break;
                     case 3:
                         //Administracion
                         break;
                     case 4:
                         //Gestion
-                        if(isset($_GET['item']) && !empty($_GET['item'])) {
+                        if (isset($_GET['item']) && !empty($_GET['item'])) {
                             $item = $_GET['item'];
 
                             switch ($item) {
@@ -217,6 +217,21 @@ require_once "../includes/navbarPanelControl.php";
                         //Pestaña de moderador
                         if ($user->getTipo() < 1) {
                             header("Location: ../index.php");
+                        } else {
+                            require_once "../Classes/Listador.php";
+
+                            $listaVideos = Listador::listarVideoReportados();
+
+                            for ($i = 0; $i < sizeof($listaVideos); $i++) {
+                                $video = $listaVideos[$i];
+                                echo '<div class="itemRow">
+                                <img class="moderacionTableElement" src="../' . $video->getMiniatura() . '">
+                                <p class="moderacionTableElement">' . $video->getTitulo() . '</p>
+                                <p class="moderacionTableElement">' . $video->getNombreUsuario() . '</p>
+                                <div id="acceptVideoBtn" class="actionIcon moderacionTableElement" data-acceptedvideo="'.$video->getId().'"><img src="../imgs/acceptButtonIcon.png" class="icon"></div>
+                                <div id="rejectVideoBtn" class="actionIcon moderacionTableElement" data-rejectedvideo="'.$video->getId().'"><img src="../imgs/DeleteIcon.svg" class="icon"</div>
+                              </div>';
+                            }
                         }
                         break;
                     case 3:
@@ -224,11 +239,11 @@ require_once "../includes/navbarPanelControl.php";
                         break;
                     case 4:
                         //Pestaña de gestion
-                        if($user->getTipo() < 2) {
+                        if ($user->getTipo() < 2) {
                             header("Location: ../index.php");
                         } else {
                             //Comprobamos si el usuario ha seleccionado algun elemento en el panel de gestion
-                            if(isset($_GET['item']) && !empty($_GET['item']) && is_numeric($_GET['item'])) {
+                            if (isset($_GET['item']) && !empty($_GET['item']) && is_numeric($_GET['item'])) {
                                 $item = $_GET['item'];
 
                                 switch ($item) {
