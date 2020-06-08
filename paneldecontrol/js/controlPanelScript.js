cb02b70cc348ab60f9494ab3fd681c41aafffcca
alex55132 <registrostope@gmail.com>
2020-06-08 15:09:12 +0200

    Sistema de gestion de usuarios para administradores

--- paneldecontrol/js/controlPanelScript.js
+++ paneldecontrol/js/controlPanelScript.js
@@ -7,6 +7,9 @@ let gestTag;
 let acceptVideoBtn;
 let rejectVideoBtn;
 
+let goToChannelBtns;
+let deleteUserBtns;
+
 window.addEventListener('load', function () {
     //Cargamos las pestañas y le agregamos los listeners
     videoTag = document.getElementById("videoTag");
@@ -14,32 +17,82 @@ window.addEventListener('load', function () {
     admTag = document.getElementById("admTag");
     gestTag = document.getElementById("gestTag");
 
-    if(videoTag != null) {
+
+    goToChannelBtns = document.getElementsByClassName("verCanalBtn");
+    deleteUserBtns = document.getElementsByClassName("eliminarCuentaBtn");
+
+    if (goToChannelBtns != null && deleteUserBtns != null) {
+        for (let i = 0; i < goToChannelBtns.length; i++) {
+            goToChannelBtns[i].addEventListener("click", function () {
+                let channelId = this.dataset.canalid;
+                if (channelId != null && channelId != 0) {
+                    window.location.href = "../channel.php?channelId=" + channelId;
+                }
+            });
+        }
+
+        for (let a = 0; a < deleteUserBtns.length; a++) {
+            deleteUserBtns[a].addEventListener("click", function () {
+                let channelId = this.dataset.canalid;
+
+                if (channelId != null && channelId != 0) {
+                    //Si los datos son validos preparamos la peticion
+                    let deletePetition = new XMLHttpRequest();
+                    deletePetition.open("POST", "../Controllers/deleteUserController.php");
+
+                    let deleteData = new FormData();
+                    deleteData.append("channelToDeleteId", channelId.toString());
+
+                    deletePetition.onreadystatechange = function () {
+                        if (deletePetition.readyState === 4) {
+                            if (deletePetition.status === 200) {
+                                //Trabajamos con la respuesta
+                                console.log(deletePetition.responseText);
+                                let jsonResponse = JSON.parse(deletePetition.responseText);
+
+                                alert(jsonResponse.message);
+
+                                if (jsonResponse.statusCode === 0) {
+                                    location.reload();
+                                }
+                            }
+                        }
+                    }
+
+                    if (confirm("Seguro que quieres eliminar este usuario?")) {
+                        deletePetition.send(deleteData);
+                    }
+                }
+            });
+        }
+    }
+
+    if (videoTag != null) {
         videoTag.addEventListener("click", function () {
             window.location.href = "./panelDeControl.php?pestana=1";
         });
     }
 
-    if(modTag != null) {
+    if (modTag != null) {
         modTag.addEventListener("click", function () {
             window.location.href = "./panelDeControl.php?pestana=2";
         });
     }
 
-    if(admTag != null) {
+    if (admTag != null) {
         admTag.addEventListener("click", function () {
             window.location.href = "./panelDeControl.php?pestana=3";
         });
     }
 
-    if(gestTag != null) {
+    if (gestTag != null) {
         gestTag.addEventListener("click", function () {
             window.location.href = "./panelDeControl.php?pestana=4";
         });
     }
 
     acceptVideoBtn = document.getElementById("acceptVideoBtn");
-    if(acceptVideoBtn != null) {
+    if (acceptVideoBtn != null) {
         acceptVideoBtn.addEventListener("click", function () {
             let acceptPetition = new XMLHttpRequest();
 
@@ -50,10 +103,10 @@ window.addEventListener('load', function () {
             acceptPetitionData.append("videoId", acceptVideoBtn.dataset.acceptedvideo)
 
             acceptPetition.onreadystatechange = function () {
-                if(acceptPetition.readyState === 4) {
+                if (acceptPetition.readyState === 4) {
                     if (acceptPetition.status === 200) {
                         let JSONData = JSON.parse(acceptPetition.responseText);
-                        if(JSONData.statusCode === 1) {
+                        if (JSONData.statusCode === 1) {
                             alert(JSONData.message);
                             location.reload();
                         } else {
@@ -68,7 +121,7 @@ window.addEventListener('load', function () {
     }
 
     rejectVideoBtn = document.getElementById("rejectVideoBtn");
-    if(rejectVideoBtn != null) {
+    if (rejectVideoBtn != null) {
         rejectVideoBtn.addEventListener("click", function () {
             let rejectPetition = new XMLHttpRequest();
 
@@ -79,10 +132,10 @@ window.addEventListener('load', function () {
             rejectPetitionData.append("videoId", rejectVideoBtn.dataset.rejectedvideo);
 
             rejectPetition.onreadystatechange = function () {
-                if(rejectPetition.readyState === 4) {
+                if (rejectPetition.readyState === 4) {
                     if (rejectPetition.status === 200) {
                         let JSONData = JSON.parse(rejectPetition.responseText);
-                        if(JSONData.statusCode === 1) {
+                        if (JSONData.statusCode === 1) {
                             alert(JSONData.message);
                             location.reload();
                         } else {
